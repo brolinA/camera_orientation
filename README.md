311bd9d8860a6ac0bc6a8027816d92eb6e3c71f2
brolin <a.brolin@hitechroboticsystemz.com>
2019-10-14 15:51:42 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# camera_orientation Package
+This package will let you find the **orientation of any RGBD camera** (Eg. Microsoft Kinect and Realsense camera series by Intel). This simple package will help to find the pitch and yaw of the camera and use it for various purpose.
+
+### Intention
+A cropbox is used to filter the region of interest in an input pointcloud and the output pointcloud is used for further operations. This package was written with the intention to stablize this output pointcloud by calculating the pitch angle of the camera and adjusting the rotation of the cropbox w.r.t this pitch angle so that the output pointcloud is always stable irrespective of the pitch of the camera.
+
+### Math involved
+The basic concept here is to find the angle enclosed between a reference plane and a plane extacted from the input pointcloud. 
+
+Given two planes,
+
+### <p align="center">Plane1: ![equation](img/eq1.png)</p>
+
+### <p align="center">Plane2: ![equation](img/eq2.png)</p>
+
+The angle(&theta;) between plane1 and plane2 is given by the equation,
+
+#### <p align="center">![equation](img/main.png)</p>
+
+Depending on what is being calculated (i.e, **pitch or yaw**), the reference plane can be either floor or the wall (See Fig 1). The Pitch and Yaw angles are shown in **Fig 2** and **Fig 3** respectively
+
+Assuming all coefficients of reference plane as 1 for simplicity, the plane equtions can be reduced as,
+### <p align="center">Reference plane:![equation](img/ref.png)</p>
+
+### <p align="center">Floor/Wall plane:![equation](img/fl_wl.png)</p>
+
+We will get the values of a<sub>2</sub>, b<sub>2</sub>, c<sub>2</sub> usibgNow the equation for &theta; will reduce to,
+### <p align="center">![equation](img/red.png)</p>
+
+### <p align="center"><img src="img/full_depiction.jpg" width="350" height="250" align="center"></img><br>***Fig 1:Camera setup and description*** </p>
+
+### <p align="center"><img src="img/Pitch.jpg" width="350" height="250"></img><br>***Fig 2:Pitch angle between floor plane & plane extracted using RANSAC*** </p>
+
+### <p align="center"><img src="img/Yaw.jpg" width="250" height="350"></img><br>***Fig 3:Yaw angle between wall plane & plane extracted using RANSAC***</p>
+
+### Working with this package
+
+You can clone the package using the following command,
+
+```
+$ git clone https://github.com/brolinA/camera_orientation.git
+```
+
+###Parameters
+
+Before launching the file make sure you have updated the following launch parameters depending on your need.
+
+```html
+1. <remap from="/camera/depth/color/points" to="/camera/depth/color/points" />
+2. <param name="find_pitch" value="true"/>
+3. <param name="find_yaw" value="false"/>
+```
+In line 1, remap the pointcloud topic to the appropriate one. Set the appropriate parameter (find_pitch or find_yaw)to true depending on what you would like to calculate i.e, Pitch or Yaw. Set both parameters to true if you want to calculate both at the same time
+
+Launch the node using,
+
+```
+roslaunch camera_orientation camera_orientation.launch
+```
+
+The **Pitch and Yaw angle** will be published in **/pitch_angle** and **/yaw_angle** topics respectively
---
+++ src/camera_orientation.cpp
@@ -0,0 +1,168 @@
+#include "ros/ros.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "geometry_msgs/PointStamped.h"
+#include "pcl/PointIndices.h"
+#include "pcl_ros/point_cloud.h"
+#include "pcl/point_types.h"
+#include "pcl_conversions/pcl_conversions.h"
+#include "pcl/common/angles.h"
+#include "pcl/sample_consensus/method_types.h"
+#include "pcl/sample_consensus/model_types.h"
+#include "pcl/segmentation/sac_segmentation.h"
+#include "pcl/filters/extract_indices.h"
+#include <pcl/filters/crop_box.h>
+#include<cmath>
+#include<math.h>
+
+
+ros::Publisher floor_cloud_pub, wall_cloud_pub, yaw_angle, pitch_angle;
+geometry_msgs::PointStamped pitch, yaw;
+bool find_pitch, find_yaw;
+
+double angle_measurement(pcl::PointCloud<pcl::PointXYZ>::Ptr agg_cloud_pcl, int i)
+{
+
+    pcl::PointIndices::Ptr indices(new pcl::PointIndices());
+
+    pcl::PointIndices indices_internal;
+		pcl::SACSegmentation<pcl::PointXYZ> seg;
+		seg.setOptimizeCoefficients(true);
+		// Search for a plane perpendicular to some axis (specified below).
+		seg.setModelType(pcl::SACMODEL_PERPENDICULAR_PLANE);
+		seg.setMethodType(pcl::SAC_RANSAC);
+		// Set the distance to the plane for a point to be an inlier.
+		seg.setDistanceThreshold(0.005);
+		seg.setInputCloud(agg_cloud_pcl);
+
+		
+		/*
+		Note:
+		This axis is with respect to the camera_depth_optical_frame of the camera which
+		is z-axis points outside the camera and y-axis pointing towards the ground fronm
+		the center of the IR emitter.
+
+		To find pitch of the camera, find the plane that is perpendicular to y-axis of 
+		camera_depth_optical_frame i.e, the floor
+			use " axis << 0, 0, 1; "
+
+		To find yaw of the camera, find the plane that is perpendicular to z-axis of 
+		camera_depth_optical_frame i.e, the wall in front of the camera
+			use " axis << 0, 1, 0; "
+
+		*/
+
+		Eigen::Vector3f axis;
+		
+		if (i == 1)
+			axis << 0, 0, 1;
+		else
+			axis << 0, 1, 0;
+		
+		seg.setAxis(axis);
+		seg.setEpsAngle(pcl::deg2rad(10.0));
+
+		// coeff contains the coefficients of the plane:
+		// ax + by + cz + d = 0
+		pcl::ModelCoefficients coeff;
+		seg.segment(indices_internal, coeff);
+
+		double a, b, c;
+
+		a = coeff.values[0];
+		b = coeff.values[1];
+		c = coeff.values[2];
+
+		ROS_INFO("Coefficients are : %lf %lf %lf ", a, b, c);
+
+		//angle between two planes taking a plane which is in x-y plane
+		//i.e, having normal from the plane as z-axis.
+		double slope, rad_, deg_;
+
+		slope = (c)/sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2));
+
+    rad_ = acos(slope);
+
+		deg_ = rad_ * 180/3.1415; 
+
+	//	ROS_INFO("Angle : %1.3lf ", deg_);
+
+
+		*indices = indices_internal;
+
+		if (indices->indices.size() == 0) {
+		  ROS_ERROR("Unable to find surface.");
+		  //return;
+			}
+	
+	pcl::PointCloud<pcl::PointXYZ>::Ptr subset_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+	// Extract subset of original_cloud into subset_cloud:
+	pcl::ExtractIndices<pcl::PointXYZ> extract;
+	extract.setInputCloud(agg_cloud_pcl);
+	extract.setIndices(indices);
+	extract.filter(*subset_cloud);
+
+	if (i ==1)
+	{
+    wall_cloud_pub.publish(*subset_cloud);
+		yaw.header.stamp = ros::Time::now();
+		yaw.point.x = deg_;
+		yaw_angle.publish(yaw);
+	}
+	else
+	{
+    floor_cloud_pub.publish(*subset_cloud);
+		pitch.header.stamp = ros::Time::now();
+    pitch.point.x = deg_;
+		pitch_angle.publish(pitch);
+    ROS_INFO("Pitch Angle : %1.3lf ", deg_);
+	}
+  return rad_;
+}
+
+void CameraCallback(const sensor_msgs::PointCloud2& msg)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::fromROSMsg(msg, *cloud);
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr yaw_cloud_pcl(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr pitch_cloud_pcl(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr final_cloud_pcl(new pcl::PointCloud<pcl::PointXYZ>);
+
+  pcl::CropBox < pcl::PointXYZ > yaw_filter;
+	yaw_filter.setMin(Eigen::Vector4f(-1.0, -0.5, -0.7, 1.0));
+	yaw_filter.setMax(Eigen::Vector4f(1.0, 0.5, 1.5, 1.0));
+	yaw_filter.setInputCloud(cloud);
+	yaw_filter.filter(*yaw_cloud_pcl);
+
+	pcl::CropBox < pcl::PointXYZ > pitch_filter;
+	pitch_filter.setMin(Eigen::Vector4f(-1.0, -0.05, -0.7, 1.0));
+	pitch_filter.setMax(Eigen::Vector4f(1.0, 0.5, 1.5, 1.0));
+	pitch_filter.setInputCloud(cloud);
+	pitch_filter.filter(*pitch_cloud_pcl);
+
+	if(find_pitch)
+	  angle_measurement(pitch_cloud_pcl,2);
+
+	if (find_yaw)
+		angle_measurement(yaw_cloud_pcl,1);
+
+}
+void load_parameters(ros::NodeHandle nh_)
+{
+	nh_.getParam("/find_pitch", find_pitch);
+	nh_.getParam("/find_yaw", find_yaw);
+}
+int main(int argc, char** argv) 
+{
+  ros::init(argc, argv, "camera_orientation_node");
+  ros::NodeHandle nh;
+	load_parameters(nh);
+  floor_cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("floor_plane", 1, true);
+  wall_cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("wall_plane", 1, true);
+  yaw_angle = nh.advertise<geometry_msgs::PointStamped>("yaw_angle",1,true);
+  pitch_angle = nh.advertise<geometry_msgs::PointStamped>("pitch_angle",1,true);
+  ros::Subscriber sub = nh.subscribe("/camera/depth/color/points", 1, CameraCallback);
+  ros::spin();
+  return 0;
+}
